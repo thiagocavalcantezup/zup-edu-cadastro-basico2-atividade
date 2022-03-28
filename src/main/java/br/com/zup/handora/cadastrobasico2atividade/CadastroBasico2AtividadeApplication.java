d76b747fa24ce31a89333088cc67574839b2045e
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 11:08:17 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrobasico2atividade/CadastroBasico2AtividadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrobasico2atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBasico2AtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBasico2AtividadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/handora/cadastrobasico2atividade/CadastroBasico2AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrobasico2atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroBasico2AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
