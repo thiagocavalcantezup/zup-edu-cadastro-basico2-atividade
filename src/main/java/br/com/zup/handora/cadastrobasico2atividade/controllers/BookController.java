9cff4e29b7a70aec335931f3d234bca94f3c0735
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 12:10:10 -0300

    Add book repository dependency to book controller

--- src/main/java/br/com/zup/handora/cadastrobasico2atividade/controllers/BookController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico2atividade/controllers/BookController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.cadastrobasico2atividade.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico2atividade.repositories.BookRepository;
+
 @RestController
 @RequestMapping(BookController.BASE_URI)
 public class BookController {
 
     public final static String BASE_URI = "/books";
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
 }
