9a8fc2f80f65d443b470ce3bf6a344156b4bd256
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 12:07:38 -0300

    Add Book entity class

---
+++ src/main/java/br/com/zup/handora/cadastrobasico2atividade/models/Book.java
@@ -0,0 +1,49 @@
+package br.com.zup.handora.cadastrobasico2atividade.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.ISBN;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, length = 4000)
+    private String description;
+
+    @Column(nullable = false)
+    @Past
+    private LocalDate publicationDate;
+
+    @ISBN
+    private String isbn;
+
+    public Book() {}
+
+    public Book(String title, String description, LocalDate publicationDate, String isbn) {
+        this.title = title;
+        this.description = description;
+        this.publicationDate = publicationDate;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
