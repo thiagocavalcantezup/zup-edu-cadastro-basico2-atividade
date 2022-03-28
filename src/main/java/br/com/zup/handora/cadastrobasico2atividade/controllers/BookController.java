5dfadfb2896f3a1880f7990078b84434407661ec
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-28 12:23:33 -0300

    Add book creation method to book controller

--- src/main/java/br/com/zup/handora/cadastrobasico2atividade/controllers/BookController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico2atividade/controllers/BookController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.cadastrobasico2atividade.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.cadastrobasico2atividade.models.Book;
+import br.com.zup.handora.cadastrobasico2atividade.models.BookDTO;
 import br.com.zup.handora.cadastrobasico2atividade.repositories.BookRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class BookController {
 
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postMethodName(@RequestBody @Valid BookDTO bookDTO,
+                                               UriComponentsBuilder uriComponentsBuilder) {
+        Book book = bookRepository.save(bookDTO.toBook());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(book.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
